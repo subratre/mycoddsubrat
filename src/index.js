e0844d3dc1425e1a454936f2d3208cc89f1e1758
subrat.panda@muvi.com <subrat.panda@muvi.com>
2022-01-10 20:37:04 +0530

    all commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Greetings() {
+  return <h4>This is subrat and it my first component</h4>;
+}
+
+ReactDOM.render(<Greetings />, document.getElementById("root"));
